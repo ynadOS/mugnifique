7ba15f0512830b17495a23fa16792633bf2e5764
Sodany Sean <champloo63@gmail.com>
2024-11-13 10:33:33 +0100

    minor changes front

--- front/src/components/Banner.jsx
+++ front/src/components/Banner.jsx
@@ -6,11 +6,9 @@ function Banner() {
         <div className="hero min-h-screen mb-20" style={{backgroundImage: `url(${backgroundImage})`}}>
             <div className="hero-content text-neutral-content  bg-[#A1C6EA] rounded-lg p-12  mx-auto md:ml-auto md:mr-8 lg:mr-24">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <h1 className="mb-5 text-5xl font-bold">Bienvenue</h1>
                     <p className="mb-5">
-                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-                    excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id
-                    nisi.
+                    Bienvenue sur notre site de mugs personnalisables ! Créez le mug qui vous ressemble ou offrez un cadeau unique, avec des designs et textes à personnaliser selon vos envies. À chaque tasse, son histoire !
                     </p>
                     <button className="btn bg-[#E5E5E5] text-[#A1C6EA] border-none">Voir Les nouvelles collections</button>
 
